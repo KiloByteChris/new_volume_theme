182a26114ac92fb2757c9a33a7c14a2a741b158c
c.mcguire <cdmcguire000@gmail.com>
2018-05-23 16:15:30 -0700

    footer styling

--- footer.php
+++ footer.php
@@ -15,22 +15,24 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'new_volume' ) ); ?>">
+			<p>
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'new_volume' ) ); ?>">
 				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'new_volume' ), 'WordPress' );
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__( 'Proudly powered by %s', 'new_volume' ), 'WordPress' );
 				?>
-			</a>
-			<span class="sep"> | </span>
+				</a>
+			</p>
+			<!-- <span class="sep"> | </span> -->
+			<p>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'new_volume' ), 'new_volume', '<a href="http://underscores.me/">Underscores.me</a>' );
 				?>
+			</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
-
 <?php wp_footer(); ?>
-
 </body>
 </html>
