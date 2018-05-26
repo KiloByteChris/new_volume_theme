080dda66139efec2ddff6d0533d6ce0785fba327
c.mcguire <cdmcguire000@gmail.com>
2018-05-26 14:36:28 -0700

    styled the links

--- sidebar.php
+++ sidebar.php
@@ -16,8 +16,14 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
 
-<?php if ( is_active_sidebar( 'contact-form' ) ) : ?>
+<?php /*if ( is_active_sidebar( 'contact-form' ) ) : ?>
 	<div id="contact-form-sidebar" class="contact-form-sidebar widget-area" role="complementary">
 		<?php dynamic_sidebar( 'contact-form' ); ?>
 	</div><!-- #primary-sidebar -->
+
 <?php endif; ?>
+<?php if ( is_active_sidebar( 'contact-info-about' ) ) : ?>
+		<div id="contact-info-about" class="contact-info-about-sidebar widget-area" role="complementary">
+			<?php dynamic_sidebar( 'contact-info-about' ); ?>
+		</div><!-- #primary-sidebar -->
+		<?php endif; */?>
